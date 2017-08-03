1ac46faeb439058a28aa87b12889af6b460c3b1b
nightrunner <690732060@qq.com>
2017-08-03 15:03:28 +0800

    加入common-service-base模块

--- ui-admin/src/main/java/org/nr/tour/AdminUIApplication.java
+++ ui-admin/src/main/java/org/nr/tour/AdminUIApplication.java
@@ -13,30 +13,10 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 @EnableHystrix
 @EnableFeignClients
 @SpringBootApplication
-//@EnableSwagger2
 public class AdminUIApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AdminUIApplication.class, args);
     }
-
-//    @Bean
-//    public Docket createRestApi() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .apiInfo(apiInfo())
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("org.nr"))
-//                .paths(PathSelectors.any())
-//                .build();
-//    }
-//
-//    private ApiInfo apiInfo() {
-//        return new ApiInfoBuilder()
-//                .title("Spring Boot中使用Swagger2构建RESTful APIs")
-//                .description("更多Spring Boot相关文章请关注：http://blog.didispace.com/")
-//                .termsOfServiceUrl("http://blog.didispace.com/")
-//                .version("1.0")
-//                .build();
-//    }
 
 }
